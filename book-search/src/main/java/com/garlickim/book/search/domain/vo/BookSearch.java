ba56b9d356314fe0f384ce6bd002a9542ddd9600
gaeul kim <kimgaeul4@gmail.com>
2019-11-03 20:07:33 +0900

    BookSearch 도메인에 Builder 추가시 누락된 annotation 추가

--- book-search/src/main/java/com/garlickim/book/search/domain/vo/BookSearch.java
+++ book-search/src/main/java/com/garlickim/book/search/domain/vo/BookSearch.java
@@ -1,10 +1,14 @@
 package com.garlickim.book.search.domain.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookSearch
 {
     private String  type;
